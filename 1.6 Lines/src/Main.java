e22d0e23c36f04d252a074a739366fd0a136b8bc
Ивн <defalt5162@gmail.com>
2025-03-23 14:42:50 +0300

    String и метод .replace

--- 1.6 Lines/src/Main.java
+++ 1.6 Lines/src/Main.java
@@ -1,7 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        lesson1();
     }
-    //
+
+    public static void lesson1() {
+        String address = "Адрес, который мы хотим указать при доставке";
+        String street = "манежная";
+        street = "манежная площадь";                                  // String строка — неизменяемый объект. Если нужно изменить строку, то нужно создать новую.
+        String streetTrue = street.replace("м", "М");
+        System.out.println(street);
+        System.out.println("Адрес магазина - " + streetTrue);             // не суммируются, а «склеиваются»
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }

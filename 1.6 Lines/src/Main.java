40a359a63f874526a8cbb56fc428160b0e1c2db9
Ивн <defalt5162@gmail.com>
2025-03-26 21:21:17 +0300

    HW done task1-3

--- 1.6 Lines/src/Main.java
+++ 1.6 Lines/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         lesson1();
         task1();
+        task2();
+        task3();
     }
 
     public static void lesson1() {
@@ -25,24 +27,46 @@ public class Main {
         } else if (phone.length() == 11 && phone.charAt(0) != '7') {        // Если в строке ровно 11 символов И если 1-й элемент строки не равен 7
             throw new RuntimeException("Среди нас посторонний");            // Метод .charAt() - берёт определённый символ из строки. Номер символа указывается в ()
         }
-
         System.out.println("phone = " + phone);
+        String expectingPhone = "79608769845";
+        if (phone.equals(expectingPhone)) {                                 // Сравниваем phone с expectedPhone с помощью метода equals()
+            System.out.println("Успех");
+        } else {
+            System.out.println("Неудача");
+        }
 
+        if (phone.equalsIgnoreCase(expectingPhone)) {                                 // Метод equalsIgnoreCase без учёта регистра
+            System.out.println("Успех");
+        } else {
+            System.out.println("Неудача");
+        }
+//        if (phone.isEmpty()) { Печатаем ошибку и просим пользователя ввести телефон}
+//          Метод isBlank() тоже проверяет, равна ли длина строки 0. Но в отличии от isEmpty(), isBlank()  проверяет еще на наличие пробелов:
 
+//        Метод contains() проверяет последовательность символов:
+//        Метод endsWith() / startsWith проверяет, на что заканчивается / начинается строка:
 
 
     }
 
     public static void task1() {
-
-
-
-
-
-
-
-
-
+        String firstName = "Ivanov ";
+        String middleName = "Ivan ";
+        String lastName = "Ivanovich";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
+    }
+    public static void task2() {
+        String firstName = "Ivanov ";
+        String middleName = "Ivan ";
+        String lastName = "Ivanovich";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    }
+    public static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 
 
